15e7ba68864f1ee6116ff874a12cc21abe6f7cbe
Yunji Choi <youmcord@gmail.com>
2021-03-27 13:52:12 +0900

    [최윤지] 최초 프로젝트 구성

---
+++ src/main/java/com/mulcam/ai/SpringBootGitTestCyjApplication.java
@@ -0,0 +1,13 @@
+package com.mulcam.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGitTestCyjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGitTestCyjApplication.class, args);
+	}
+
+}
